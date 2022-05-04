c4cbae4a5a49d43e5b075e3f3aa4961dfececd6f
Michael Tew <mike.tew@gmail.com>
2022-05-04 02:41:27 -0700

    WIP: GUI layout setup.

--- main.py
+++ main.py
@@ -11,6 +11,24 @@ class Gui(tk.Tk):
         self.title("Wordle Clone")
         self.geometry("+900+300")
 
+        self.guesses = [["a" for _ in range(5)] for _ in range(6)]
+        print(self.guesses)
+
+        self.prev_guess_frame = ttk.LabelFrame(self, text="Guesses")
+        self.prev_guess_frame.pack()
+
+        self.display_board()
+
+        self.btn = ttk.Button(self.prev_guess_frame, text="Test")
+        self.btn.pack()
+
+    def display_board(self):
+        for row in self.guesses:
+            row_frame = ttk.Frame(self.prev_guess_frame)
+            row_frame.pack()
+            for char in row:
+                ttk.Label(row_frame, text=char, font="Helvetica 30").pack(side="left")
+
 
 if __name__ == "__main__":
     with open("word_list.pkl", "rb") as open_file:
