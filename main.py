7a2309ce14b7f4047db15653210d312c82ad750d
Michael Tew <mike.tew@gmail.com>
2022-05-11 02:14:33 -0700

    WIP: Improving layout.

--- main.py
+++ main.py
@@ -8,26 +8,38 @@ class Gui(tk.Tk):
     def __init__(self, word):
         super().__init__()
 
+        self.NUM_GUESSES = 6
+
         self.title("Wordle Clone")
         self.geometry("+900+300")
 
-        self.guesses = [["a" for _ in range(5)] for _ in range(6)]
-        print(self.guesses)
+        self.board = [["-" for _ in range(5)] for _ in range(self.NUM_GUESSES)]
+        print(self.board)
 
         self.prev_guess_frame = ttk.LabelFrame(self, text="Guesses")
         self.prev_guess_frame.pack()
 
         self.display_board()
 
-        self.btn = ttk.Button(self.prev_guess_frame, text="Test")
-        self.btn.pack()
+        self.guess_entry = tk.Entry(self.prev_guess_frame, font="Helvetica 15")
+        self.guess_entry.pack(side="left", padx=20, pady=20)
+        self.guess_button = ttk.Button(
+            self.prev_guess_frame, text="Guess", width=20, command=self.make_guess
+        )
+        self.guess_button.pack(side="right", padx=[0, 20])
 
     def display_board(self):
-        for row in self.guesses:
+        for row in self.board:
             row_frame = ttk.Frame(self.prev_guess_frame)
             row_frame.pack()
             for char in row:
-                ttk.Label(row_frame, text=char, font="Helvetica 30").pack(side="left")
+                ttk.Label(row_frame, text=char, font="Helvetica 30").pack(
+                    side="left", padx=10
+                )
+
+    def make_guess(self):
+        guess = self.guess_entry.get()
+        print(guess)
 
 
 if __name__ == "__main__":
