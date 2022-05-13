60cafeaf7e8ab592516cb92c9dfb075e6ce92e86
Michael Tew <mike.tew@gmail.com>
2022-05-13 06:38:07 -0700

    Improved styling.

--- main.py
+++ main.py
@@ -8,58 +8,66 @@ class Gui(tk.Tk):
     def __init__(self, word_list):
         super().__init__()
 
-        self.NUM_GUESSES = 6
-        self.guess_number = 0
-        self.word_list = word_list
-        self.word = choice(word_list).upper()
-        print(self.word)
-
         self.title("Wordle Clone")
-        self.geometry("630x450+900+300")
+        self.geometry("630x600+900+300")
+        self.NUM_GUESSES = 6
+        self.word_list = word_list
 
-        self.board = [["__" for _ in range(5)] for _ in range(self.NUM_GUESSES)]
-        self.board_frame = ttk.LabelFrame(self)
-        self.display_board()
-
+        self.board_frame = tk.Frame(self)
         guess_frame = tk.Frame(self)
-        guess_frame.place(x=20, y=350)
+        guess_frame.grid(row=1, column=0)
         self.guess_entry = tk.Entry(guess_frame, font="Helvetica 15")
         self.guess_entry.pack(side="left", padx=20, pady=20)
         self.guess_button = ttk.Button(
             guess_frame, text="Guess", width=15, command=self.make_guess
         )
         self.guess_button.pack(side="left", padx=[0, 20])
-        self.new_word_button = ttk.Button(
-            guess_frame, text="New Word", width=15, command=self.on_new_word
+        new_word_button = ttk.Button(
+            guess_frame, text="New Word", width=15, command=self.game_setup
         )
-        self.new_word_button.pack(side="left")
+        new_word_button.pack(side="left")
+
+        self.word_label = ttk.Label(
+            self, width=10, anchor="center", font="Helvetica 30"
+        )
+        self.word_label.grid()
+
+        self.game_setup()
 
     def display_board(self):
         self.board_frame.destroy()
-        self.board_frame = ttk.LabelFrame(self, text="Guesses")
-        self.board_frame.place(x=100, y=10)
+        self.board_frame = tk.Frame(self, background="#121213")
+        self.board_frame.grid(row=0, column=0, ipadx=50, ipady=20)
 
         for row in self.board:
-            row_frame = ttk.Frame(self.board_frame)
+            row_frame = tk.Frame(self.board_frame, bg="#121213")
             row_frame.pack()
+
             for char in zip(row, self.word):
+                border_color = tk.Frame(row_frame)
+                border_color.pack(side="left", padx=3, pady=3)
                 color = self.get_color(char)
                 ttk.Label(
-                    row_frame,
+                    border_color,
                     text=char[0],
-                    foreground="white",
+                    foreground="#FFFFFF",
                     background=color,
-                    font="Helvetica 30",
+                    font="Courier 30 bold",
                     anchor="center",
-                ).pack(side="left", ipadx=20)
+                ).pack(ipadx=20, padx=3, pady=3)
+
+                color = "#3A3A3C" if char[0] == " " else color
+                border_color.config(bg=color)
 
     def get_color(self, char):
         if char[0] == char[1]:
-            return "green"
+            return "#538D4E"
         elif char[0] in self.word:
-            return "yellow"
+            return "#B59F3B"
+        elif char[0] == " ":
+            return "#121213"
         else:
-            return "black"
+            return "#3A3A3C"
 
     def make_guess(self):
         guess = self.guess_entry.get().upper()
@@ -77,16 +85,22 @@ class Gui(tk.Tk):
 
     def on_win(self):
         print("Victory")
+        self.guess_button.config(state="disabled")
+        self.word_label.config(text=self.word)
 
     def on_lose(self):
         print("Lost")
+        self.guess_button.config(state="disabled")
+        self.word_label.config(text=self.word)
 
-    def on_new_word(self):
-        print("New Word")
-        self.random_word()
-
-    def random_word(self):
-        print(word_list)
+    def game_setup(self):
+        self.guess_button.config(state="normal")
+        self.word_label.config(text="")
+        self.guess_number = 0
+        self.word = choice(word_list).upper()
+        self.board = [[" " for _ in range(5)] for _ in range(self.NUM_GUESSES)]
+        self.display_board()
+        print(self.word)
 
 
 if __name__ == "__main__":
